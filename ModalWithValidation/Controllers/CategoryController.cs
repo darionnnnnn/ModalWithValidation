8975743a6d9f255ff887897cbd9ff2b742dfd989
darionnnnnn <wayneeeeee@gmail.com>
2018-11-24 22:02:22 +0800

    Modal With Validation

---
+++ ModalWithValidation/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModalWithValidation.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            NorthwindEntities nwEntities = new NorthwindEntities();
+
+            var data = nwEntities.Categories.Take(20);
+
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categories category)
+        {
+            NorthwindEntities nwEntities = new NorthwindEntities();
+
+            if (category != null && ModelState.IsValid)
+            {
+                nwEntities.Categories.Add(category);
+
+                nwEntities.SaveChanges();
+
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(category);
+            }
+        }
+    }
+}
---
+++ ModalWithValidation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModalWithValidation.MvcApplication" Language="C#" %>
